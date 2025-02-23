021d0300ce0c192809712d441d3f550aa6b91e76
linard999 <g_linard@hotmail.com>
2025-02-23 11:29:39 -0300

    feat: Adição da funcionalidade de player de música

---
+++ data.js
@@ -0,0 +1,17 @@
+let songs = [
+    {
+        name: 'Doomsday',
+        path: 'musicas/MF DOOM - Doomsday (feat. Pebbles The Invisible Girl) [Official Audio].mp3',
+        artist: 'MF DOOM'
+    },
+    {
+        name: 'Tomara',
+        path: 'musicas/Pablo - Tomara ( Pablo 20 Anos).mp3',
+        artist: 'Pablo'
+    },
+    {
+        name: 'Ouriço',
+        path: 'musicas/Juliano Holanda - Ouriço.mp3',
+        artist: 'Juliano Holanda'
+    }
+]
