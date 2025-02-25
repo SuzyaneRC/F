729275bcc3f5810a9b1f866a9730eba160f85ace
SuzyaneRC <suzyanetrabalhos@gmail.com>
2025-02-24 23:17:55 -0300

    ajustes

--- shows.js
+++ shows.js
@@ -1,3 +1,17 @@
+function formatarData(dataString) {
+    const datas = new Date(dataString);
+    
+    return datas.toLocaleDateString('pt-BR', {
+        year: 'numeric',  
+        month: 'numeric',    
+        day: 'numeric' ,
+        hour:  'numeric',
+        minute:'numeric'
+        
+    });
+}
+
+
 fetch('https://fasc2025.onrender.com/shows').then(
     res => {
         return res.json();
@@ -15,8 +29,8 @@ divIndivual.classList.add('showsDiv');
             <h1 id="tituloDiv">${user.name}</h1>
             <p>${user.description}</p>
             <p>Tipo de atração: ${user.type}</p>
-            <p> Início: ${user.startDate} </p>
-            <p>Término: ${user.endDate}</p>
+            <p> Início: ${formatarData(user.startDate)} </p>
+            <p>Término: ${formatarData(user.endDate)} </p>
             <p>Local: ${user.location}</p>
         
         `;
